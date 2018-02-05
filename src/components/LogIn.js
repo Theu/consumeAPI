df3fea41361cbf977a30075cde90a21c6b30ec29
Matteo Contini <matteo.contini@zalando-lounge.de>
2018-02-05 09:56:08 +0100

    configured router, pats and links

---
+++ src/components/LogIn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link} from 'react-router-dom';
+
+class LogIn extends React.Component {
+
+    render() {
+        return (
+            <Link to="todo">
+                <h1>I am a log in</h1>
+            </Link>
+        )
+    }
+}
+
+export default LogIn;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 
-import registerServiceWorker from './registerServiceWorker';
+import LogIn from './components/LogIn';
 
 
 const store = configureStore();
 render(
         <Provider store={store}>
-                <App />
-        </Provider>, document.getElementById('root'));
-registerServiceWorker();
+                <BrowserRouter>
+                        <div>
+                                <Route exact path='/' component={LogIn} />
+                                <Route exact path='/todo' component={App} />
+                        </div>
+                </BrowserRouter>
+        </Provider>
+, document.getElementById('root'));
