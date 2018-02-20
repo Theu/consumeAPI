3746a01289125d083645ecbd1ed778bd352f4df0
Theu <info@matteocontini.com>
2018-02-20 16:13:14 +0100

    removed unesed packages import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Route } from 'react-router';
 import configureStore from './redux/store/configureStore';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-
-import LogIn from './components/LogIn';
 
 const store = configureStore();
 render(
@@ -16,4 +12,3 @@ render(
     <App />
   </Provider>
   , document.getElementById('root'));
-  
