88f0bfc5c9080e0bcb3859fcb097a5f68210e652
Theu <info@matteocontini.com>
2018-02-20 15:51:35 +0100

    rmoved possible error

--- src/components/ErrorHandler.js
+++ src/components/ErrorHandler.js
@@ -8,7 +8,7 @@ class ErrorHandler extends React.Component {
 
         return (
             <div className='errorHandler-wrapper'>
-                <div>We can't satisfy your request because we got {failureReason}.</div>
+                <div>We cann not satisfy your request because we got {failureReason}.</div>
                 <div className='toggle-alert'
                 onClick={this.toggleError}>Got it</div>
             </div>
